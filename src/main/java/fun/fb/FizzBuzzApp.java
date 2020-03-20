a8ed60378cc52d2d356d566d952e39d817b3948f
Neeraj <neerajmariyana@hotmail.com>
2020-03-20 01:44:52 +0000

    FizzBuzz cleanup

--- src/main/java/fun/fb/FizzBuzzApp.java
+++ src/main/java/fun/fb/FizzBuzzApp.java
@@ -5,7 +5,7 @@ import fun.fb.algo.FizzBuzzSimple;
 public class FizzBuzzApp {
 
     public static void main(String args[]) {
-        final FizzBuzzSimple fbSimple = new FizzBuzzSimple(0);
+        final FizzBuzzSimple fbSimple = new FizzBuzzSimple();
         for (int i = 1; i <= 100; i++) {
             byte index = fbSimple.next();
             System.out.println(fbSimple.translate(index).orElse(String.valueOf(i)));
